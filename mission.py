688032adc01ded682255156dd8753b1dd769f6d9
kyle Sawyer <ksawyer@ualberta.ca>
2017-01-14 13:37:36 -0700

    Finished gen message

--- mission.py
+++ mission.py
@@ -1,4 +1,6 @@
 from enum import Enum
+from waypointobject import Waypoint
+from config import *
 
 class InsertMode(Enum):
     Append = 1
@@ -21,7 +23,7 @@ class NavPattern(Enum):
 class Mission:
     """class to define missions applying to 1 or more waypoints"""
     
-    def __init__(self, name, index, duration, nav_pattern, waypoints, radius=None):
+    def __init__(self, name, index, duration, nav_pattern, waypoints, radius = None):
         assert isinstance(nav_pattern, NavPattern)
         assert nav_pattern != NavPattern.MISSION_CIRCLE and nav_pattern != NavPattern.MISSION_CIRCLE_LLA or radius is not None
         
@@ -35,68 +37,67 @@ class Mission:
     def get_nav_pattern(self):
         return self._nav_pattern
     
-    def set_nav_pattern(self, nav_pattern):
+    def set_nav_pattern(self, nav_pattern, radius = None):
         assert isinstance(nav_pattern, NavPattern)
-        self._nav_pattern = nav_pattern;
+        print("setting nav pattern")
+        self._nav_pattern = nav_pattern
     
     nav_pattern = property(get_nav_pattern, set_nav_pattern)
     
     def gen_mission_msg(self, ac_id, insert_mode = InsertMode.Append):
+        assert isinstance(insert_mode, InsertMode)
         msg = pprzmsg("datalink", self._nav_pattern.name)
 
         msg['ac_id'] = ac_id
-        msg['insert'] = insert_mode
+        msg['insert'] = insert_mode.name
         msg['duration'] = self.duration
+        msg['index'] = self.index
 
         if self._nav_pattern == NavPattern.MISSION_GOTO_WP_LLA:
             assert len(self.waypoints) == 1
-            waypoint = self.waypoints[0].get_latlon;
+            waypoint = self.waypoints[0].get_latlon()
             msg['wp_lat'] = waypoint['lat']
             msg['wp_lon'] = waypoint['lon']
             msg['wp_alt'] = waypoint['alt']
 
         elif self._nav_pattern == NavPattern.MISSION_CIRCLE_LLA:
             assert len(self.waypoints) == 1
-            waypoint = self.waypoints[0].get_latlon;
+            if not self.radius: 
+                raise AttributeError("Circle requires radius")
+            waypoint = self.waypoints[0].get_latlon()
 
             msg['center_lat'] = waypoint['lat']
             msg['center_lon'] = waypoint['lon']
             msg['center_alt'] = waypoint['alt']
             msg['radius'] = self.radius
             
-        elif msg_id == 'MISSION_SEGMENT_LLA':
+        elif self._nav_pattern == NavPattern.MISSION_SEGMENT_LLA:
             assert len(self.waypoints) == 2
-            waypoint1 = self.waypoints[0].get_latlon
-            waypoint2 = self.waypoints[1].get_latlon
+            waypoint1 = self.waypoints[0].get_latlon()
+            waypoint2 = self.waypoints[1].get_latlon()
             msg['segment_lat_1'] = waypoint1['lat']
             msg['segment_lon_1'] = waypoint1['lon']
             msg['segment_lat_2'] = waypoint2['lat']
             msg['segment_lon_2'] = waypoint2['lon']
 
-        #elif msg_id == 'MISSION_PATH_LLA':
-        #    msg['point_lat_1'] = msgs.get('point_lat_1')
-        #    msg['point_lon_1'] = msgs.get('point_lon_1')
-        #    msg['point_lat_2'] = msgs.get('point_lat_2')
-        #    msg['point_lon_2'] = msgs.get('point_lon_2')
-        #    msg['point_lat_3'] = msgs.get('point_lat_3')
-        #    msg['point_lon_3'] = msgs.get('point_lon_3')
-        #    msg['point_lat_4'] = msgs.get('point_lat_4')
-        #    msg['point_lon_4'] = msgs.get('point_lon_4')
-        #    msg['point_lat_5'] = msgs.get('point_lat_5')
-        #    msg['point_lon_5'] = msgs.get('point_lon_5')
-        #    msg['path_alt'] = msgs.get('path_alt')
-        #    msg['nb'] = msgs.get('nb')
+        elif self._nav_pattern == NavPattern.MISSION_PATH_LLA:
+            waypoints_num = len(self.waypoints)
+            assert waypoints_num >= 2 and waypoints_num <= 5
+            for i in range(0,waypoints_num):
+                waypoint = self.waypoints[i].get_latlon()
+                msg['point_lat_' + str(i + 1)] = waypoint['lat']
+                msg['point_lon_' + str(i + 1)] = waypoint['lon']
+            msg['path_alt'] = self.waypoints[0].get_latlon()['alt']
+            msg['nb'] = waypoints_num
 
-        #elif msg_id == 'MISSION_SURVEY_LLA':
-        #    msg['survey_lat_1'] = msgs.get('survey_lat_1')
-        #    msg['survey_lon_1'] = msgs.get('survey_lon_1')
-        #    msg['survey_lat_2'] = msgs.get('survey_lat_2')
-        #    msg['survey_lon_2'] = msgs.get('survey_lon_2')
-        #    msg['survey_alt'] = msgs.get('survey_alt')
+        elif self._nav_pattern == NavPattern.MISSION_SURVEY_LLA:
+            assert len(self.waypoints) == 2
+            waypoint1 = self.waypoints[0].get_latlon()
+            waypoint2 = self.waypoints[1].get_latlon()
+            msg['survey_lat_1'] = waypoint1['lat']
+            msg['survey_lon_1'] = waypoint1['lon']
+            msg['survey_lat_2'] = waypoint2['lat']
+            msg['survey_lon_2'] = waypoint2['lon']
+            msg['survey_alt'] = waypoint1['alt']
         
         return msg
-    
-a = Mission("name", 1, -1, NavPattern.MISSION_GOTO_WP, [1, 2])
-
-a.nav_pattern = NavPattern.MISSION_GOTO_WP_LLA
-print(a.nav_pattern)
