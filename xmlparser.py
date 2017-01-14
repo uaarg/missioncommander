9babc8677297874e7fe654086a9eb41e278abe7a
Lionfrog <bwerner@ualberta.ca>
2017-01-13 21:28:12 -0800

    Added Pi and xml parser files, still need to fix them. Also made addtobstacle and addtomission python scripts YO!

---
+++ xmlparser.py
@@ -0,0 +1,40 @@
+import xml.etree.ElementTree as ET
+
+class xmlreader:
+    def __init__(self):
+        self.waypoints = []
+        self.commands = []
+        self.stagedlist = []
+        self.openfile("sampleFlightPlan.xml")
+
+    def openfile(self,  filepath, callback=None):
+        tree = ET.parse(filepath)
+        root = tree.getroot()
+
+
+        for waypoint in root.iter('waypoint'):
+            self.waypoints.append(waypoint.attrib)
+
+
+        i = 0
+        for command in root.iter('command'):
+            self.commandmsg = ""
+            print (command.keys())
+            for element in command.keys():
+                print (element)
+                if (element != "id" and element != "name"):
+                    self.commandmsg = self.commandmsg + element + "=" + command.get(element) + " "
+
+            self.commands.append(command.attrib)
+            self.commands[i]['msg'] = self.commandmsg
+            i = i+1
+        print (self.commands)
+        print(self.stagedlist)
+
+        for staged in root.iter('staged_commands'):
+            self.stagedlist.append(staged.attrib)
+
+
+
+        if callback != None :
+            callback(self.commands)
