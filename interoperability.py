ae7b281af2d1e6a218f627fe7e0678db67d7e190
Cindy Xiao <cndxiao@gmail.com>
2017-01-20 21:52:56 -0700

    Cleanup of interop file, add MissionInformation class.

--- interoperability.py
+++ interoperability.py
@@ -14,6 +14,48 @@ from pprzlink.message import PprzMessage
 from database import AirplaneTelemetry
 from obstacle import Obstacle
 
+class MissionInformation():
+    """
+    Class for obtaining information about a mission from the interoperability server, then sending it over the Ivy bus.
+    Args:
+        interopclient: An interoperability client object to handle sending data to the interoperability server.
+    """
+    def __init__(self, interopclient, ivysender):
+        self.interopclient = interopclient
+        self.ivysender = ivysender
+        self.mission_info = {}
+
+    def getMissionInformation(self):
+        """
+        Gets mission information from the interoperability server.
+        """
+        try:
+            self.mission_info = interopclient.get_missions()
+        except InteropError as error:
+            print(error.message)
+        
+    def sendIvyEmergentTarget(self, ac_id, emergent_waypoint_id, altitude):
+        """
+        Sends updated information about the location of the emergent target.
+        This is dependent on there being an existing waypoint in the flight plan that can be used for the emergent target task.
+        Args:
+            ac_id: The aircraft ID, an integer.
+            emergent_waypoint_id: The id of the waypoint associated with the emergent target, an integer.
+            altitude: The desired MSL altitude for the emergent target waypoint, in metres.
+        """
+        msg = PprzMessage("ground", "SHAPE")
+        msg['wp_id'] = emergent_waypoint_id
+        msg['ac_id'] = ac_id
+        msg['lat'] = int(
+            self.mission_info["emergent_last_known_pos"]["latitude"] * 1e7
+        )
+        msg['lon'] = int(
+            self.mission_info["emergent_last_known_pos"]["longitude"] * 1e7
+        )
+        msg['alt'] = int(altitude * 1e3)
+
+        self.ivysender(msg)
+
 class TelemetryThread(Thread):
     """
     Thread to send telemetry information.
@@ -41,39 +83,41 @@ class TelemetryThread(Thread):
         is being updated with telemetry at.
         """
 
-def sendIvyShapeMessage(obstacle_id, obstacle, ivysender):
-    """
-    Send a shape message over the Ivy bus for a given obstacle.
-
-    Args:
-        obstacle_id: The ID of the obstacle.
-        obstacle: An Obstacle object.
-        ivysender: A callback to a function used to send Ivy messages, given a message dictionary.
-    """
-    msg = PprzMessage("ground", "SHAPE")
-    msg['id'] = obstacle_id
-    msg['linecolor'] = "white"
-    msg['fillcolor'] = "red" if obstacle.moving else "orange"
-    msg['opacity'] = 1
-    msg['shape'] = 0
-    msg['status'] = 0
-    msg['latarr'] = [int(obstacle.lat * 10000000.)] * 2
-    msg['lonarr'] = [int(obstacle.lon * 10000000.)] * 2
-    msg['radius'] = obstacle.geom_data['radius']
-    if obstacle.shape == 'cylinder':
-        msg['text'] = "%.2fm" % (obstacle.geom_data['height'])
-    if obstacle.shape == 'sphere':
-        msg['text'] = "%.2fm" % (obstacle.geom_data['altitude'])
-
-    ivysender(msg)
-
 class ObstacleThread(Thread):
     """
     Thread to query for obstacle information,
     update the program's internal store of obstacles,
     and send obstacle information over the Ivy bus.
     """
-    def __init__(self, interopclient, ivysender, updateFrequency=2):
+
+    @staticmethod
+    def _sendIvyShapeMessage(obstacle_id, obstacle, ivysender):
+        """
+        Send a shape message over the Ivy bus for a given obstacle.
+
+        Args:
+            obstacle_id: The ID of the obstacle.
+            obstacle: An Obstacle object.
+            ivysender: A callback to a function used to send Ivy messages, given a message dictionary.
+        """
+        msg = PprzMessage("ground", "SHAPE")
+        msg['id'] = obstacle_id
+        msg['linecolor'] = "white"
+        msg['fillcolor'] = "red" if obstacle.moving else "orange"
+        msg['opacity'] = 1
+        msg['shape'] = 0
+        msg['status'] = 0
+        msg['latarr'] = [int(obstacle.lat * 10000000.)] * 2
+        msg['lonarr'] = [int(obstacle.lon * 10000000.)] * 2
+        msg['radius'] = obstacle.geom_data['radius']
+        if obstacle.shape == 'cylinder':
+            msg['text'] = "%.2fm" % (obstacle.geom_data['height'])
+        if obstacle.shape == 'sphere':
+            msg['text'] = "%.2fm" % (obstacle.geom_data['altitude'])
+
+        ivysender(msg)
+
+    def __init__(self, interopclient, ivysender, update_frequency=2):
         """
         Constructor for an ObstacleThread.
 
@@ -86,7 +130,7 @@ class ObstacleThread(Thread):
         self.interopclient = interopclient
         self.ivysender = ivysender
         self.obstacles = []
-        self.sleepDuration = 1.0 / updateFrequency
+        self.sleepDuration = 1.0 / update_frequency
 
     def run(self):
         while True:
